6ceda43539e6bede5f94e0ede69d890befc5518e
najmisidiq <najmisidiq@gmail.com>
2026-05-11 00:40:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import googlemaps
-from flask import Flask, request, jsonify
+send_from_statement
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
